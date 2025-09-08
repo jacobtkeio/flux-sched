4bc6e70caf312b4a04faece6dc712c79644b36d2
jacobtkeio <jacobtkeio@gmail.com>
2025-09-08 12:12:53 -0700

    rq: Add "within" argument to match commands
    Problem: The traverser supports "within" match logic, which filters out
    matches that start too far into the future, but this feature is
    inaccessible from resource-query.
    
    Add a "within" option to match * requests as a 4th argument. Rather than
    an FSD value, this must be a unitless integer time value.
    
    e.g., match allocate /path/to/jobspec 60 will not return resource sets
    that start more than 60 (unitless time) seconds in the future.

--- resource/utilities/command.cpp
+++ resource/utilities/command.cpp
@@ -310,7 +310,7 @@ int cmd_match (std::shared_ptr<detail::resource_query_t> &ctx,
 {
     match_op_t match_op;
 
-    if (args.size () != 3) {
+    if (args.size () < 3 || args.size () > 4) {
         std::cerr << "ERROR: malformed command" << std::endl;
         return 0;
     }
@@ -323,7 +323,16 @@ int cmd_match (std::shared_ptr<detail::resource_query_t> &ctx,
     uint64_t jobid = ctx->get_job_counter ();
     std::string &jobspec_fn = args[2];
 
-    run_match (ctx, jobid, match_op, jobspec_fn, out);
+    if (args.size () == 4) {
+        try {
+            attrs.within = std::stoi (args[3]);
+        } catch (...) {
+            std::cerr << "ERROR: invalid integer for 'within' param: " << args[3] << std::endl;
+            return 0;
+        }
+    }
+
+    run_match (ctx, jobid, match_op, jobspec_fn, &attrs, out);
 
     return 0;
 }
