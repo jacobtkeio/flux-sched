53b05627a44dfb5d0e36d738030e8ea5c95b4b63
jacobtkeio <jacobtkeio@gmail.com>
2025-09-15 10:43:27 -0700

    trav: Add option to match n timeslots
    Problem: In a multicluster scheduling scheme using
    matching without allocating, it is uncertain that
    matched resources will actually be available at
    schedule-time.
    
    Solution:
    To make scheduling more likely to succeed, add the
    option to match several timeslots (without allocating).
    Then if the first choice has been claimed, the scheduler
    can choose the next timeslot. This should reduce the
    likelihood of failing to schedule a job after it
    matches successfully.

--- resource/traversers/dfu.cpp
+++ resource/traversers/dfu.cpp
@@ -399,10 +399,11 @@ int dfu_traverser_t::run (Jobspec::Jobspec &jobspec,
                           match_op_t op,
                           int64_t jobid,
                           int64_t *at,
-                          std::stringstream &o)
+                          std::stringstream &o,
+                          int64_t num_matches)
 {
     std::vector<std::shared_ptr<match_writers_t>> writers = {writer};
-    return run (jobspec, writers, op, jobid, at, o);
+    return run (jobspec, writers, op, jobid, at, o, num_matches);
 }
 
 int dfu_traverser_t::run (Jobspec::Jobspec &jobspec,
@@ -410,7 +411,8 @@ int dfu_traverser_t::run (Jobspec::Jobspec &jobspec,
                           match_op_t op,
                           int64_t jobid,
                           int64_t *at,
-                          std::stringstream &o)
+                          std::stringstream &o,
+                          int64_t num_matches)
 {
     // Clear the error message to disambiguate errors
     clear_err_message ();
@@ -419,7 +421,7 @@ int dfu_traverser_t::run (Jobspec::Jobspec &jobspec,
     graph_duration_t graph_duration = get_graph_db ()->metadata.graph_duration;
     if (!get_graph () || !get_graph_db ()
         || (get_graph_db ()->metadata.roots.find (dom) == get_graph_db ()->metadata.roots.end ())
-        || !get_match_cb () || jobspec.resources.empty ()) {
+        || !get_match_cb () || jobspec.resources.empty () || (num_matches < 1)) {
         errno = EINVAL;
         return -1;
     }
@@ -428,6 +430,7 @@ int dfu_traverser_t::run (Jobspec::Jobspec &jobspec,
     int64_t graph_end = std::chrono::duration_cast<std::chrono::seconds> (
                             graph_duration.graph_end.time_since_epoch ())
                             .count ();
+    int64_t match_time = *at;
     int64_t max_dur;
     detail::jobmeta_t meta;
     vtx_t root = get_graph_db ()->metadata.roots.at (dom);
@@ -440,7 +443,11 @@ int dfu_traverser_t::run (Jobspec::Jobspec &jobspec,
     combined_writers.push_back (vtx_writer);
 
     detail::dfu_impl_t::prime_jobspec (jobspec.resources, dfv);
-    if (meta.build (jobspec, detail::jobmeta_t::alloc_type_t::AT_ALLOC, jobid, *at, graph_duration)
+    if (meta.build (jobspec,
+                    detail::jobmeta_t::alloc_type_t::AT_ALLOC,
+                    jobid,
+                    match_time,
+                    graph_duration)
         < 0)
         return -1;
 
@@ -450,18 +457,23 @@ int dfu_traverser_t::run (Jobspec::Jobspec &jobspec,
         meta.alloc_type = jobmeta_t::alloc_type_t::AT_NO_ALLOC;
     }
 
-    if ((op == match_op_t::MATCH_SATISFIABILITY)
-        && (rc = is_satisfiable (jobspec, meta, x, root, dfv)) == 0) {
-        detail::dfu_impl_t::update ();
-    } else if ((rc = schedule (jobspec, meta, x, op, root, dfv)) == 0) {
-        *at = meta.at;
-        if (*at == graph_end) {
+    if (op == match_op_t::MATCH_SATISFIABILITY) {
+        if ((rc = is_satisfiable (jobspec, meta, x, root, dfv)) == 0)
+            detail::dfu_impl_t::update ();
+        return rc;
+    }
+
+    for (int m = 0; m < num_matches; m++) {
+        if ((rc = schedule (jobspec, meta, x, op, root, dfv)) != 0)
+            return rc;
+        match_time = meta.at;
+        if (match_time == graph_end) {
             detail::dfu_impl_t::reset_exclusive_resource_types (exclusive_types);
             // no schedulable point found even at the end of the time, return EBUSY
             errno = EBUSY;
             return -1;
         }
-        if (*at < 0 or *at > graph_end) {
+        if (match_time < 0 or match_time > graph_end) {
             detail::dfu_impl_t::reset_exclusive_resource_types (exclusive_types);
             errno = EINVAL;
             return -1;
@@ -469,40 +481,46 @@ int dfu_traverser_t::run (Jobspec::Jobspec &jobspec,
         // If job ends after the resource graph expires, reduce the duration
         // so it coincides with the graph expiration. Note that we could
         // arguably return ENOTSUP/EINVAL instead. Also note that we
-        // know *at < graph_end from the previous check, and meta.duration
+        // know match_time < graph_end from the previous check, and meta.duration
         // is < int64_t max from meta.build.
-        if ((*at + static_cast<int64_t> (meta.duration)) > graph_end)
-            meta.duration = graph_end - *at;
-        // returns 0 or -1
-        rc = detail::dfu_impl_t::update (root, combined_writers, meta);
+        if ((match_time + static_cast<int64_t> (meta.duration)) > graph_end)
+            meta.duration = graph_end - match_time;
 
+        rc = detail::dfu_impl_t::update (root, combined_writers, meta);
+        rc += detail::dfu_impl_t::reset_exclusive_resource_types (exclusive_types);
+        if (rc < 0)
+            return rc;
+
+        // Extend the duration of MWOA resources as much as possible
         if (op == match_op_t::MATCH_WITHOUT_ALLOCATING_EXTEND) {
             max_dur = maximum_duration_resources_available (get_graph_db (),
                                                             vtx_writer->get_vertices (),
-                                                            *at);
+                                                            match_time);
+            // Reached graph end, so return success
+            if (max_dur == 0)
+                return 0;
+            // A planner error occurred
             if (max_dur < 0) {
-                detail::dfu_impl_t::reset_exclusive_resource_types (exclusive_types);
-                return rc;
+                errno = EINVAL;
+                return -1;
             }
             for (auto writer : writers) {
                 // Update the matched resources with their extended, maximum duration
-                if ((rc = writer->emit_tm (*at, *at + max_dur)) < 0) {
-                    detail::dfu_impl_t::reset_exclusive_resource_types (exclusive_types);
+                if ((rc = writer->emit_tm (match_time, match_time + max_dur)) < 0)
                     return rc;
-                }
             }
+
+            // Start the next match at the end of the current one
+            meta.at = match_time + max_dur;
         }
+        // Return the first match time if there are multiple
+        if (m == 0)
+            *at = match_time;
 
-        // Returns >=0 for success
-        if ((writers[0])->emit (o) < 0)
-            errno = EIO;
-            rc = -1;
-        else
-            rc = 0;
+        // Only emit from the first writer in the list
+        if ((rc = (writers[0])->emit (o)) < 0)
+            break;
     }
-
-    // returns 0 or -1
-    rc += detail::dfu_impl_t::reset_exclusive_resource_types (exclusive_types);
 
     return rc;
 }
