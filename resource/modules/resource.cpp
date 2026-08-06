d4a94c314f9d9ffaa20c5cd726bbf51b81aed53b
jacobtkeio <jacobtkeio@gmail.com>
2026-08-06 11:53:35 -0700

    resource: Send "{}" in reconsider_blocked_jobs
    Problem: Now that the feasibility module expects a json dictionary
    format for all `sched-fluxion-resource.notify` RPCs, sending `NULL` to
    qmanager to force it to reconsider blocked jobs can cause the
    feasibility module to exit with a protocol error.
    
    Send an empty dictionary instead of NULL in reconsider_blocked_jobs.

--- resource/modules/resource.cpp
+++ resource/modules/resource.cpp
@@ -1489,7 +1489,7 @@ error:
 }
 
 /*
- * Send a NULL resource.notify message to get qmanager to reconsider jobs
+ * Send an empty resource.notify message to get qmanager to reconsider jobs
  */
 static int reconsider_blocked_jobs (
     flux_t *h,
@@ -1497,7 +1497,7 @@ static int reconsider_blocked_jobs (
 {
     int rc = 0;
     for (auto &kv : notify_msgs) {
-        if (flux_respond (h, kv.second->get_msg (), NULL) < 0) {
+        if (flux_respond (h, kv.second->get_msg (), "{}") < 0) {
             rc = -1;
             flux_log_error (h, "%s: flux_respond", __FUNCTION__);
         }
