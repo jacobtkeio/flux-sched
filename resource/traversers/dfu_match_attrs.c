b8e2b164c36709e8469b73dcb2bb90016fe0202a
jacobtkeio <jacobtkeio@gmail.com>
2026-08-10 16:36:28 -0700

    dfu: Add traversal attributes struct
    Problem: Adding new parameters to the traverser requires changing many
    function signatures across the traverser, the resource module, and the
    REAPI. For the REAPI in particular, this is a threat to ABI
    compatibility across versions. Additionally, it makes adding new
    parameters more difficult.
    
    Define a struct to hold new attributes. Pass a pointer to this struct to
    traversal functions so that new parameters can be added to the struct
    declaration without disturbing the function signatures.
    
    Move the return-by-reference arguments for traversal overhead into the
    attributes struct.

---
+++ resource/traversers/dfu_match_attrs.c
@@ -0,0 +1,32 @@
+/*****************************************************************************\
+ * Copyright 2026 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, LICENSE)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\*****************************************************************************/
+
+#include "dfu_match_attrs.h"
+
+json_t *match_attrs_to_json (struct dfu_match_attrs attrs)
+{
+    json_t *obj = NULL;
+    if (obj = json_pack ("{s:f}", "overhead", attrs.match_overhead))
+        return obj;
+    return NULL;
+}
+
+struct dfu_match_attrs *match_attrs_from_json (json_t *json)
+{
+    struct dfu_match_attrs *attrs =
+        (struct dfu_match_attrs *)malloc (sizeof (struct dfu_match_attrs));
+    if (!attrs)
+        return NULL;
+    *attrs = default_match_attrs;
+
+    if (json_unpack (json, "{s?f s?I}", "overhead", &(attrs->match_overhead)) == 0)
+        return attrs;
+    return NULL;
+}
---
+++ resource/traversers/dfu_match_attrs.h
@@ -0,0 +1,54 @@
+/*****************************************************************************\
+ * Copyright 2026 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, LICENSE)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\*****************************************************************************/
+
+#ifndef DFU_MATCH_ATTRIBUTES_H
+#define DFU_MATCH_ATTRIBUTES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <jansson.h>
+
+/*! Wrapper for optional traversal attributes.
+ *  Passing around a pointer to dfu_match_attrs allows adding new attributes
+ *  without breaking ABI compatibility.
+ *    match_overhead     Double to store performance overhead in terms of
+ *                       elapsed time needed to complete the match operation.
+ *                       Not used by traverser.
+ */
+struct dfu_match_attrs {
+    double match_overhead;
+};
+
+const struct dfu_match_attrs default_match_attrs = {0.0f};
+
+/*! Encode a dfu_match_attrs struct as a json object.
+ *  The caller must decref the new json object.
+ *  \param attrs    Attribute struct to encode.
+ *  \return         Newly allocated json object representing attrs;
+ *                  NULL on error.
+ */
+json_t *match_attrs_to_json (struct dfu_match_attrs attrs);
+
+/*! Decode a json object as a dfu_match_attrs struct.
+ *  The caller must free the new struct pointer.
+ *  \param json     Json object to decode.
+ *  \return         Newly allocated, populated struct ptr;
+ *                  NULL on error.
+ */
+struct dfu_match_attrs *match_attrs_from_json (json_t *json);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // DFU_MATCH_ATTRIBUTES_H
